77eaaeed93bd6fc58906ef4d44278de6965469f7
Andrea <andrea.menegat@gmail.com>
2015-06-04 15:13:51 +0200

    aggiunte le icone, i layout, adapter e helper delle notifiche

---
+++ Pointrest/src/com/pointrestapp/pointrest/PreferitoHelper.java
@@ -0,0 +1,22 @@
+package com.pointrestapp.pointrest;
+
+import android.provider.BaseColumns;
+
+public class PreferitoHelper implements BaseColumns{
+	public static final String TABLE_NAME = "preferiti";
+	public static final String PREFERITO = "preferito";
+
+	/*
+	
+	public static final String IMMAGINE = "immagine";
+	
+	public static final String CREATE_QUERY =
+			"CREATE TABLE " + TABLE_NAME +
+			" ( " + 
+			_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			PREFERITO + " TEXT NOT NULL" +
+			IMMAGINE + "!?" +
+			");";
+	
+	*/
+}
