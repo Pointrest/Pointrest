82cea3b9e26aefe8e1ee1b171ded6f531259487c
Andrea <andrea.menegat@gmail.com>
2015-06-17 13:39:17 +0200

    fixato tutti adapter e versione bata di fragment funzionante

---
+++ Pointrest/src/com/pointrestapp/pointrest/DialogNotificheBloccate.java
@@ -0,0 +1,56 @@
+package com.pointrestapp.pointrest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class DialogNotificheBloccate extends DialogFragment {
+	public static DialogNotificheBloccate getInstance(long id){
+		DialogNotificheBloccate vItem = new DialogNotificheBloccate(id);
+		Bundle bundle = new Bundle();
+	    vItem.setArguments(bundle);
+		return vItem;
+	}
+	
+	private long tmpID;
+	
+	private DialogNotificheBloccate(long id){
+		tmpID= id;
+	}
+	
+	@Override
+	public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		
+		
+		//Bundle bundle = getArguments();
+		
+		builder.setTitle("Ripristino");
+		
+		
+		builder.setMessage("Vuoi ripristinare l'elemento?");
+		
+		builder.setPositiveButton("Ripristina", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Intent preferito = new Intent();
+				preferito.putExtra("ID", tmpID);
+				
+				getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, preferito);				
+			}
+		}); 
+		
+		builder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, tipologia);
+			}
+		});
+		return builder.create();
+	}
+}
