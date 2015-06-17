bbc9a08d7d2b7004b87ce74dd5114836449c8951
miloradstevanovic <misa.kg@gmail.com>
2015-06-17 13:42:03 +0200

    reformed db

--- Pointrest/src/com/pointrestapp/pointrest/data/PuntiImagesDbHelper.java
+++ Pointrest/src/com/pointrestapp/pointrest/data/PuntiImagesDbHelper.java
@@ -5,13 +5,11 @@ import android.provider.BaseColumns;
 public class PuntiImagesDbHelper implements BaseColumns  {
 	
 	public static final String TABLE_NAME = "punti_images";
-	public static final String SERVER_ID = "server_id";
 	public static final String PUNTO_ID = "punto_id";
 	
 	public static final String CREATE_QUERY = 
 			"CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
-			+ _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-			+ SERVER_ID +  " INTEGER NOT NULL, "
+			+ _ID + " INTEGER PRIMARY KEY, "
 			+ PUNTO_ID +  " INTEGER NOT NULL "
 			+ ");";
 }
